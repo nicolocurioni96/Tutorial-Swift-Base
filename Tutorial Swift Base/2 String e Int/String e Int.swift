128a608ef282853b6cdb889bf3582bb4f319bb4a
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-12 14:38:20 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

--- Tutorial Swift Base/2 String e Int/String e Int.swift
+++ Tutorial Swift Base/2 String e Int/String e Int.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct String_e_Int: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello, World!")
+                .padding()
+        }
     }
 }
 
