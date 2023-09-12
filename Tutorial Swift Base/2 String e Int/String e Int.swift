7f87cf2b0751161eadd6edae8a893c3827f53b9d
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-12 14:35:30 +0200

    Added the `2 String e Int` folder with the relative Swift file

---
+++ Tutorial Swift Base/2 String e Int/String e Int.swift
@@ -0,0 +1,20 @@
+//
+//  String e Int.swift
+//  Tutorial Swift Base
+//
+//  Created by Nicolò Curioni on 12/09/23.
+//
+
+import SwiftUI
+
+struct String_e_Int: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct String_e_Int_Previews: PreviewProvider {
+    static var previews: some View {
+        String_e_Int()
+    }
+}
