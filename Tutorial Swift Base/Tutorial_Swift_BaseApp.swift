56a51160469b8992d738648562e2b08874eb6358
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-12 11:37:42 +0200

    First commit

---
+++ Tutorial Swift Base/Tutorial_Swift_BaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_Swift_BaseApp.swift
+//  Tutorial Swift Base
+//
+//  Created by Nicolò Curioni on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_Swift_BaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
