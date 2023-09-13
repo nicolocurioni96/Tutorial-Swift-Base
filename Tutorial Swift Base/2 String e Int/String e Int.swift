444094a9a379f1a4211133915c6278548e3a1c57
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-13 14:14:52 +0200

    Added the `String e Int` content in the tutorial with the `Strings`

--- Tutorial Swift Base/2 String e Int/String e Int.swift
+++ Tutorial Swift Base/2 String e Int/String e Int.swift
@@ -8,9 +8,63 @@
 import SwiftUI
 
 struct String_e_Int: View {
+    // String literals
+    let aString = ""
+    
+    // Multiline Strings literals
+    let aMultilineString = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris bibendum nibh et nisl accumsan, vitae ullamcorper ligula mattis.
+    
+    Nam vestibulum risus semper mauris egestas pharetra.
+    
+    Aliquam rutrum euismod nibh non tincidunt. Nam hendrerit dolor semper mi vehicula faucibus. Sed eget neque ac arcu lacinia auctor ac et enim. Maecenas auctor laoreet massa in posuere.
+    """
+    
+    // Special Characters in String Literals
+    var aStringWithBackslash = "\\" // Add backslash in a String
+    var aStringWithDoubleQuotationMark = "\"" // Add a " in a String
+    
+    var theAmzingWorldOfCoding = "I love coding, to make money and change the World!"
+    let dollarSign = "\u{24}" // $
+    let sparklingHeart = "\u{1F496}" // 💖
+    
+    var emptyString = ""
+    var anotherEmptyString = String()
+    
+    // String mutability
+    var aVariableHelloString = "Hello,"
+    var aVariableWorldString = "World!"
+    var helloWorldString = ""
+    var theAmzingWorldOfCodingTotalChararacters = 0
+    var theAmzingWorldOfCodingTotalChararactersString = ""
+    
+    init() {
+        /// String check with`isEmpty` vs `count == 0`
+        /*if emptyString.isEmpty {
+         print("Nothing to see here...")
+         }
+         
+         if emptyString.count == 0 {
+         print("Nothing to see here...")
+         }*/
+        
+        theAmzingWorldOfCoding = "I \(sparklingHeart) coding, to make \(dollarSign) and change the World!"
+        
+        // String mutability
+        //helloWorldString += aVariableHelloString + " " + aVariableWorldString
+        
+        // String Interpolation
+        //helloWorldString = "\(aVariableHelloString) \(aVariableWorldString)"
+        
+        // Number of characters in a String
+        theAmzingWorldOfCodingTotalChararacters = theAmzingWorldOfCoding.count
+        
+        theAmzingWorldOfCodingTotalChararactersString = "The number of characters in the \"theAmzingWorldOfCoding\" string contains \(theAmzingWorldOfCodingTotalChararacters) characters."
+    }
+    
     var body: some View {
         VStack {
-            Text("Hello, World!")
+            Text(theAmzingWorldOfCodingTotalChararactersString)
                 .padding()
         }
     }
