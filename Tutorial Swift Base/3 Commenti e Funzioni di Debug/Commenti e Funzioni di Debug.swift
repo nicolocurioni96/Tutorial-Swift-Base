268f83f922297ff9dc1d43adb6f91e8e6bb55da1
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-14 12:42:19 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Base/3 Commenti e Funzioni di Debug/Commenti e Funzioni di Debug.swift
@@ -0,0 +1,28 @@
+//
+//  Commenti e Funzioni di Debug.swift
+//  Tutorial Swift Base
+//
+//  Created by Nicolò Curioni on 14/09/23.
+//
+
+import SwiftUI
+
+struct Commenti_e_Funzioni_di_Debug: View {
+    
+    init() {
+        
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Hello, World")
+                .padding()
+        }
+    }
+}
+
+struct Commenti_e_Funzioni_di_Debug_Previews: PreviewProvider {
+    static var previews: some View {
+        Commenti_e_Funzioni_di_Debug()
+    }
+}
