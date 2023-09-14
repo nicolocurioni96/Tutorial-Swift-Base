afce4152f855ac3966546ade13aa25b6da82b53b
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-14 14:02:01 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Base/4 Double e Float/Double e Float.swift
@@ -0,0 +1,23 @@
+//
+//  Double e Float.swift
+//  Tutorial Swift Base
+//
+//  Created by Nicolò Curioni on 14/09/23.
+//
+
+import SwiftUI
+
+struct Double_e_Float: View {
+    var body: some View {
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .padding()
+        }
+    }
+}
+
+struct Double_e_Float_Previews: PreviewProvider {
+    static var previews: some View {
+        Double_e_Float()
+    }
+}
