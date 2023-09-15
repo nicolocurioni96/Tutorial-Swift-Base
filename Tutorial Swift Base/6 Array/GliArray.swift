e11f85e67aef9c2bda104669a9d1c5aec9cf524b
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-15 19:21:14 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Base/6 Array/GliArray.swift
@@ -0,0 +1,23 @@
+//
+//  GliArray.swift
+//  Tutorial Swift Base
+//
+//  Created by Nicolò Curioni on 15/09/23.
+//
+
+import SwiftUI
+
+struct GliArray: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+        }
+    }
+}
+
+struct GliArray_Previews: PreviewProvider {
+    static var previews: some View {
+        GliArray()
+    }
+}
