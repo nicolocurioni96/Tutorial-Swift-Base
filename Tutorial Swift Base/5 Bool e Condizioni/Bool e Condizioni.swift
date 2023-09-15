b92920aa1798a77f9e16870aa497dbf7160b519f
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-15 16:37:55 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Base/5 Bool e Condizioni/Bool e Condizioni.swift
@@ -0,0 +1,23 @@
+//
+//  Bool e Condizioni.swift
+//  Tutorial Swift Base
+//
+//  Created by Nicolò Curioni on 15/09/23.
+//
+
+import SwiftUI
+
+struct Bool_e_Condizioni: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+        }
+    }
+}
+
+struct Bool_e_Condizioni_Previews: PreviewProvider {
+    static var previews: some View {
+        Bool_e_Condizioni()
+    }
+}
