8f086408ce572527acc0940ee1dbd28c432b256a
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-16 15:52:35 +0200

    Added the `Array` content to the tutorial

--- Tutorial Swift Base/6 Array/GliArray.swift
+++ Tutorial Swift Base/6 Array/GliArray.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 struct GliArray: View {
+    /// `Array` a list of items, of the same type, in an ordered list
+    var names: [String] = ["Tim"]
+    
+    init() {
+        // Add an item
+        names.append("John")
+        names.append("Mario")
+        names.append("Lisa")
+        
+        // Remove an item at index
+        // (uncomment to test)
+        names.remove(at: 2) // Mario has been removed!
+        
+        // Create an Array with a Default Value
+        let fourIntegers = Array(repeating: 27, count: 5)
+        
+        print(fourIntegers)
+        
+        names[1] = "Steve"
+        
+    }
+    
     var body: some View {
         VStack {
-            Text("Hello, World!")
-                .padding()
+            ForEach(names, id: \.self) { name in
+                Text("Hello, \(String(name))")
+                    .padding()
+            }
         }
     }
 }
