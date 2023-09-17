7be86d13f8f4f9c3c5f9e549f1fda6c2aecf9a53
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-17 16:45:51 +0200

    Added the `Loop` content to the tutorial

--- Tutorial Swift Base/7 Loop/Cicli Loop.swift
+++ Tutorial Swift Base/7 Loop/Cicli Loop.swift
@@ -8,6 +8,35 @@
 import SwiftUI
 
 struct Cicli_Loop: View {
+    /// `Control Flow`
+    /// `for-in`
+    /// `while`
+    /// `repeat-while`
+    let names = ["Alex", "Tim", "Anna", "Lisa"]
+    let numberOfLegs = ["dog": 4, "spider": 8, "ant": 6]
+    var number = 1
+    
+    init() {
+        /*for name in names {
+         print("Hello, \(name)")
+         }*/
+        
+        /*for (animaleName, legCount) in numberOfLegs {
+         print("\(animaleName)s have \(legCount) count")
+         }*/
+        
+        /*while number <= 27 {
+         number += 1
+         print(number)
+         print("The number is now equal to 27 🙃!")
+         }*/
+        
+        /*repeat {
+         print(number)
+         number += 1
+         } while number <= 20*/
+    }
+    
     var body: some View {
         VStack {
             Text("Hello, World!")
