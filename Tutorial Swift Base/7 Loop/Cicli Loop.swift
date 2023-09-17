a19f34cfb780493755a4669e684056361bc24100
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-09-17 15:58:06 +0200

    Improved the layout by adding a vertical stack and a padding to the `Text` UI component

---
+++ Tutorial Swift Base/7 Loop/Cicli Loop.swift
@@ -0,0 +1,23 @@
+//
+//  Cicli Loop.swift
+//  Tutorial Swift Base
+//
+//  Created by Nicolò Curioni on 17/09/23.
+//
+
+import SwiftUI
+
+struct Cicli_Loop: View {
+    var body: some View {
+        VStack {
+            Text("Hello, World!")
+                .padding()
+        }
+    }
+}
+
+struct Cicli_Loop_Previews: PreviewProvider {
+    static var previews: some View {
+        Cicli_Loop()
+    }
+}
